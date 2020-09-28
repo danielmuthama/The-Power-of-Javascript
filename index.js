53e04277d0580f6c970efe3b8b4d4c4f3656c825
Daniel Muthama <40577986+danielmuthama@users.noreply.github.com>
2020-09-28 15:01:54 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const jsonfile = require('jsonfile');
+const moment = require('moment');
+const simpleGit = require('simple-git');
+const random = require('random');
+const makecommit = n => {
+    if(n===0) return simpleGit().push();
+    const x = random.int(0,54);
+    const y = random.int(0,6);
+    const FILE_PATH = './data.json';
+    const DATE = moment().subtract(2, 'y').add(1, 'd').add(x, 'w').add(y, 'd').format();
+    const data = {
+        date: DATE
+    }
+     jsonfile.writeFile(FILE_PATH, data, ()=>{
+        simpleGit().add([FILE_PATH]).commit(DATE, {'--date': DATE},
+         makecommit.bind(this, --n));
+     });
+}
+
+makecommit(300);
